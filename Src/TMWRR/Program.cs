80f4f63c5ef3767e7ce81ff6e292b5d11c71b7a4
Petr Pivoňka <petrpiv1@gmail.com>
2025-03-20 21:41:58 +0100

    Add base deps

---
+++ Src/TMWRR/Program.cs
@@ -0,0 +1,105 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using Serilog.Sinks.SystemConsole.Themes;
+using Serilog;
+using Scalar.AspNetCore;
+using HealthChecks.UI.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddOutputCache();
+builder.Services.AddHybridCache();
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = 429;
+});
+
+builder.Services.AddHealthChecks();
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(theme: AnsiConsoleTheme.Sixteen, applyThemeToRedirectedOutput: true)
+    .WriteTo.OpenTelemetry()
+    .CreateLogger();
+
+builder.Services.AddSerilog();
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(options =>
+    {
+        options
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddProcessInstrumentation()
+            .AddOtlpExporter();
+
+        options.AddMeter("System.Net.Http");
+    })
+    .WithTracing(options =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            options.SetSampler<AlwaysOnSampler>();
+        }
+
+        options
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation()
+            .AddOtlpExporter();
+    });
+builder.Services.AddMetrics();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapOpenApi();
+app.MapScalarApiReference(options =>
+{
+    options.DefaultHttpClient = new(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    options.Theme = ScalarTheme.DeepSpace;
+});
+
+app.UseRateLimiter();
+
+app.MapHealthChecks("/_health", new()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+}).RequireAuthorization();
+
+app.UseOutputCache();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public partial class Program;
