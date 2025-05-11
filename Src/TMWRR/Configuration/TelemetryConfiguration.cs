72ad2d105393d72a2405eb161da0755e5cfee748
Petr Pivoňka <petrpiv1@gmail.com>
2025-05-11 17:02:16 +0200

    Remove AddMetrics

--- Src/TMWRR/Configuration/TelemetryConfiguration.cs
+++ Src/TMWRR/Configuration/TelemetryConfiguration.cs
@@ -56,6 +56,5 @@ public static class TelemetryConfiguration
                     .AddEntityFrameworkCoreInstrumentation()
                     .AddOtlpExporter();
             });
-        services.AddMetrics();
     }
 }
