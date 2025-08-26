4e3815dd9c6fe7e44aa66abdd30bbf2a3423c88e
Petr Pivoňka <petrpiv1@gmail.com>
2025-08-26 14:21:03 +0200

    Refactor Game API

--- Src/TMWRR/Endpoints/GamesEndpoint.cs
+++ Src/TMWRR/Endpoints/GamesEndpoint.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using TMWRR.Dtos;
+using TMWRR.Dtos.TMF;
+using TMWRR.Enums;
 using TMWRR.Services;
+using TMWRR.Services.TMF;
 
 namespace TMWRR.Endpoints;
 
@@ -9,10 +12,16 @@ public static class GamesEndpoint
     public static void Map(RouteGroupBuilder group)
     {
         group.MapGet("/", GetGames);
-        group.MapGet("/{id}", GetGame);
+        group.MapGet("/{gameId}", GetGame);
 
-        GamesTMFLoginsEndpoint.Map(group.MapGroup("tmf/logins"));
-        GamesTMFCampaignsEndpoint.Map(group.MapGroup("tmf/campaigns"));
+        group.MapGet("/{gameId}/campaigns", GetGameCampaigns);
+        group.MapGet("/{gameId}/campaigns/{campaignId}", GetGameCampaign);
+        // group.MapGet("/{gameId}/campaigns/{campaignId}/maps")
+        group.MapGet("/{gameId}/campaigns/{campaignId}/maps/{mapUid}/records", GetGameCampaignRecordsByMapUid);
+        group.MapGet("/{gameId}/campaigns/{campaignId}/snapshots/latest", GetLatestGameCampaignSnapshot);
+        group.MapGet("/{gameId}/campaigns/{campaignId}/snapshots/{createdAt}/records/{mapUid}", GetGameCampaignSnapshotRecordsByMapUid);
+
+        group.MapGet("/{gameId}/logins/{loginId}", GetGameLogin);
     }
 
     private static async Task<Ok<IEnumerable<GameDto>>> GetGames(IGameService gameService, CancellationToken cancellationToken)
@@ -22,18 +31,191 @@ public static class GamesEndpoint
         return TypedResults.Ok(dtos);
     }
 
-    private static async Task<Results<Ok<GameDto>, ValidationProblem, NotFound>> GetGame(string id, IGameService gameService, CancellationToken cancellationToken)
+    private static async Task<Results<Ok<GameDto>, ValidationProblem, NotFound>> GetGame(
+        string gameId, 
+        IGameService gameService, 
+        CancellationToken cancellationToken)
     {
-        if (id.Length > 12)
+        if (gameId.Length > 12)
         {
             var errors = new Dictionary<string, string[]>
             {
-                [nameof(id)] = ["The game ID length must not exceed 12 characters."]
+                [nameof(gameId)] = ["The game ID length must not exceed 12 characters."]
             };
             return TypedResults.ValidationProblem(errors);
         }
 
-        var dto = await gameService.GetDtoAsync(id, cancellationToken);
+        var dto = await gameService.GetDtoAsync(gameId, cancellationToken);
+
+        if (dto is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        return TypedResults.Ok(dto);
+    }
+
+    private static async Task<Results<Ok<IEnumerable<TMFCampaignDto>>, ValidationProblem>> GetGameCampaigns(
+        string gameId, 
+        ICampaignService campaignService, 
+        CancellationToken cancellationToken)
+    {
+        if (!gameId.Equals(nameof(EGame.TMF), StringComparison.InvariantCultureIgnoreCase))
+        {
+            var errors = new Dictionary<string, string[]>
+            {
+                [nameof(gameId)] = ["Only 'TMF' is supported as game ID."]
+            };
+            return TypedResults.ValidationProblem(errors);
+        }
+
+        var dtos = await campaignService.GetAllDtosAsync(cancellationToken);
+
+        return TypedResults.Ok(dtos);
+    }
+
+    private static async Task<Results<Ok<TMFCampaignDto>, ValidationProblem, NotFound>> GetGameCampaign(
+        string gameId, 
+        string campaignId,
+        ICampaignService campaignService, 
+        CancellationToken cancellationToken)
+    {
+        var errors = new Dictionary<string, string[]>();
+        if (!gameId.Equals(nameof(EGame.TMF), StringComparison.InvariantCultureIgnoreCase))
+        {
+            errors[nameof(gameId)] = ["Only 'TMF' is supported as game ID."];
+        }
+        if (campaignId.Length > 32)
+        {
+            errors[nameof(campaignId)] = ["The campaign ID length must not exceed 32 characters."];
+        }
+        if (errors.Count > 0)
+        {
+            return TypedResults.ValidationProblem(errors);
+        }
+
+        var dto = await campaignService.GetDtoAsync(campaignId, cancellationToken);
+
+        if (dto is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        return TypedResults.Ok(dto);
+    }
+
+    private static async Task<Results<Ok<IEnumerable<TMFCampaignScoresRecordDto>>, ValidationProblem>> GetGameCampaignRecordsByMapUid(
+        string gameId,
+        string campaignId, 
+        string mapUid,
+        IScoresSnapshotService scoresSnapshotService,
+        CancellationToken cancellationToken)
+    {
+        var errors = new Dictionary<string, string[]>();
+        if (!gameId.Equals(nameof(EGame.TMF), StringComparison.InvariantCultureIgnoreCase))
+        {
+            errors[nameof(gameId)] = ["Only 'TMF' is supported as game ID."];
+        }
+        if (campaignId.Length > 32)
+        {
+            errors[nameof(campaignId)] = ["The campaign ID length must not exceed 32 characters."];
+        }
+        if (mapUid.Length > 32)
+        {
+            errors[nameof(mapUid)] = ["The MapUid length must not exceed 32 characters."];
+        }
+        if (errors.Count > 0)
+        {
+            return TypedResults.ValidationProblem(errors);
+        }
+
+        var dtos = await scoresSnapshotService.GetLatestRecordDtosAsync(campaignId, mapUid, cancellationToken);
+
+        return TypedResults.Ok(dtos);
+    }
+
+    private static async Task<Results<Ok<TMFCampaignScoresSnapshotDto>, ValidationProblem, NotFound>> GetLatestGameCampaignSnapshot(
+        string gameId, 
+        string campaignId, 
+        IScoresSnapshotService scoresSnapshotService,
+        CancellationToken cancellationToken)
+    {
+        var errors = new Dictionary<string, string[]>();
+        if (!gameId.Equals(nameof(EGame.TMF), StringComparison.InvariantCultureIgnoreCase))
+        {
+            errors[nameof(gameId)] = ["Only 'TMF' is supported as game ID."];
+        }
+        if (campaignId.Length > 32)
+        {
+            errors[nameof(campaignId)] = ["The campaign ID length must not exceed 32 characters."];
+        }
+        if (errors.Count > 0)
+        {
+            return TypedResults.ValidationProblem(errors);
+        }
+
+        var dto = await scoresSnapshotService.GetLatestSnapshotDtoAsync(campaignId, cancellationToken);
+
+        if (dto is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        return TypedResults.Ok(dto);
+    }
+
+    private static async Task<Results<Ok<IEnumerable<TMFCampaignScoresRecordDto>>, ValidationProblem>> GetGameCampaignSnapshotRecordsByMapUid(
+        string gameId,
+        string campaignId, 
+        DateTimeOffset createdAt, 
+        string mapUid, 
+        IScoresSnapshotService scoresSnapshotService, 
+        CancellationToken cancellationToken)
+    {
+        var errors = new Dictionary<string, string[]>();
+        if (!gameId.Equals(nameof(EGame.TMF), StringComparison.InvariantCultureIgnoreCase))
+        {
+            errors[nameof(gameId)] = ["Only 'TMF' is supported as game ID."];
+        }
+        if (campaignId.Length > 32)
+        {
+            errors[nameof(campaignId)] = ["The campaign ID length must not exceed 32 characters."];
+        }
+        if (mapUid.Length > 32)
+        {
+            errors[nameof(mapUid)] = ["The MapUid length must not exceed 32 characters."];
+        }
+        if (errors.Count > 0)
+        {
+            return TypedResults.ValidationProblem(errors);
+        }
+
+        var dtos = await scoresSnapshotService.GetSnapshotRecordDtosAsync(campaignId, createdAt, mapUid, cancellationToken);
+
+        return TypedResults.Ok(dtos);
+    }
+
+    private static async Task<Results<Ok<TMFLoginDto>, ValidationProblem, NotFound>> GetGameLogin(
+        string gameId, 
+        string loginId, 
+        ILoginService loginService,
+        CancellationToken cancellationToken)
+    {
+        var errors = new Dictionary<string, string[]>();
+        if (!gameId.Equals(nameof(EGame.TMF), StringComparison.InvariantCultureIgnoreCase))
+        {
+            errors[nameof(gameId)] = ["Only 'TMF' is supported as game ID."];
+        }
+        if (loginId.Length > 32)
+        {
+            errors[nameof(loginId)] = ["The login ID length must not exceed 32 characters."];
+        }
+        if (errors.Count > 0)
+        {
+            return TypedResults.ValidationProblem(errors);
+        }
+
+        var dto = await loginService.GetTMFDtoAsync(loginId, cancellationToken);
 
         if (dto is null)
         {
